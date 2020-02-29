0ae56de287676453cba9f07d6306bc52d30bb4b3
jrpalma <jose.palma@r1soft.com>
2020-02-29 07:35:25 -0600

    Moved flagtype definition into flag.go

--- flag.go
+++ flag.go
@@ -1,5 +1,18 @@
 package arg
 
+type flagType int
+
+const (
+	unknownFlag flagType = iota
+	stringFlag
+	boolFlag
+	int64Flag
+	uint64Flag
+	float64Flag
+	enumFlag
+	argsFlag
+)
+
 type flag struct {
 	name  string
 	short string
