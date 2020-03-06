4b935c7ae0656d5cfd41d73f66a6301d7e6db3a7
jrpalma <jose.palma@r1soft.com>
2020-03-06 15:21:25 -0600

    Changed flag to option in the documentation

--- cmd.go
+++ cmd.go
@@ -6,125 +6,139 @@ import (
 
 // Cmd represents a command that will be executed by a Parser.
 // The command's Exec method is executed if a Parser can match
-// the command's prefix, name, and flags.
+// the command's prefix, name, options, and operands.
 type Cmd struct {
 	// The command's prpefix. The prefix is the string that comes
-	//before the command name and its flags.
+	//before the command name.
 	Prefix string
 	// The command's name. The name must be unique per prefix.
-	// If two commands with the same prefix and name are addOptioned
+	// If two commands with the same prefix and name are added
 	// to a Parser object, the later will replace the the earlier.
 	Name string
 	// The command's help string. The command will display the help
-	// string to document the command.
+	// if usage is displayed.
 	Help string
 	// The function that gets executed if the arguments match the command.
 	Exec func(ExecArgs) error
-	// The command's flags. Flags can be required or optional.
 
 	operands  map[int]*operand
 	shortOpts map[rune]*option
 	longOpts  map[string]*option
 }
 
-// Option adds an option. Short is a one letter option.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// Option adds an option that does not required a parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) Option(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help})
 }
 
-// ReqString adds a required string flag. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// ReqString adds a required option with a string parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) ReqString(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		required: true, dataType: String})
 }
 
-// ReqBool adds a required bool flag. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// ReqBool adds a required option with a bool parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) ReqBool(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		required: true, dataType: Bool})
 }
 
-// ReqInt64 adds a required int64 flag. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// ReqInt64 adds a required option with a int64 parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) ReqInt64(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		required: true, dataType: Int64})
 }
 
-// ReqUint64 adds a required uint64 flag. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// ReqUint64 adds a required option with a uint64 parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) ReqUint64(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		required: true, dataType: Uint64})
 }
 
-// ReqFloat64 adds a required float64 flag. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// ReqFloat64 adds a required option with a float64 parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) ReqFloat64(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		required: true, dataType: Float64})
 }
 
-// ReqEnum adds a required enum flag. Short is a one letter flag.
-// Long is the long version of the flag. Valid are the valid values
-// for this option. Help is displayed when the usage message is printed.
+// ReqEnum adds a required enum option. Valid are the valid values
+// that are acceped by this option.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) ReqEnum(short rune, long string, valid []string, help string) {
 	c.addOption(&option{short: short, long: long, help: help, valid: valid,
 		required: true, dataType: enum})
 }
 
-// OptString adds an optional string flag. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// OptString adds a optional option with a string parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) OptString(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		dataType: String})
 }
 
-// OptBool adds an optional bool flag. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// OptBool adds a optional option with a bool parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) OptBool(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		dataType: Bool})
 }
 
-// OptInt64 adds an optional int64 flag. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// OptInt64 adds a optional option with a int64 parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) OptInt64(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		dataType: Int64})
 }
 
-// OptUint64 adds an optional uint64 flag. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// OptUint64 adds a optional option with a uint64 parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) OptUint64(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		dataType: Uint64})
 }
 
-// OptFloat64 adds an optional float64 option. Short is a one letter flag.
-// Long is the long version of the flag. Help is displayed
-// when the usage message is printed.
+// OptFloat64 adds a optional option with a float64 parameter.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) OptFloat64(short rune, long string, help string) {
 	c.addOption(&option{short: short, long: long, help: help,
 		dataType: Float64})
 }
 
-// OptEnum adds an optional enum flag. Short is a one letter flag.
-// Long is the long version of the flag. Valid are the valid values
-// for this flag. Help is displayed when the usage message is printed.
+// OptEnum adds a optinal enum option. Valid are the valid values
+// that are acceped by this option.
+// Short is a one letter option. Short is not used if '0'.
+// Long is the long name for the option. Long is not used if empty.
+// Help is displayed when the usage message is printed.
 func (c *Cmd) OptEnum(short rune, long string, valid []string, help string) {
 	c.addOption(&option{short: short, long: long, help: help, valid: valid,
 		dataType: enum})
@@ -132,7 +146,8 @@ func (c *Cmd) OptEnum(short rune, long string, valid []string, help string) {
 
 // Operand adds an operand with the give position, name, and data type.
 // Position starts from zero on wards. The operand is named by name.
-// The operand data type can be specified.
+// The operand data type can be specified. Valid data types are: String,
+// Bool, Int64, Uint64, and Float64
 func (c *Cmd) Operand(position int, name string, dataType DataType) {
 	c.addOperand(&operand{position: position, name: name, dataType: dataType})
 }
